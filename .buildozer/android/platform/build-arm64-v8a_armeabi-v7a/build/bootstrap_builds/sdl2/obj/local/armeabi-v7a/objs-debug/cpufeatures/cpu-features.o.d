9854b6c2f1a5f1349eb8323473e442ca42cd79da
shararehmirbagheri <shararehmirbagheri@gmail.com>
2026-08-06 22:01:17 -0400

    Delete .buildozer/android/platform directory

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,4 +0,0 @@
-/home/sharareh/ShohrehYoga/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/sharareh/.buildozer/android/platform/android-ndk-r28c/sources/android/cpufeatures/cpu-features.c \
-  /home/sharareh/.buildozer/android/platform/android-ndk-r28c/sources/android/cpufeatures/cpu-features.h
-/home/sharareh/.buildozer/android/platform/android-ndk-r28c/sources/android/cpufeatures/cpu-features.h:
